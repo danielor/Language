d1b166593a7a96cbb00e14e5d1e3404aa8cb8a8d
Daniel Ortiz <danielor@umich.edu>
2014-11-27 12:37:19 -0500

    The pythong bindings test is integrated into the testing framework.

--- test/run.py
+++ test/run.py
@@ -19,6 +19,22 @@ files.
 """
 import argparse, os, subprocess
 
+def runListOfCommands(listOfCommands, withOutput = False, commandType = ''):
+    """
+    Run a list of commands
+    @param listOfCommands: A list of commands
+    @param withOutput: The stdout of the list of command
+    @param commandType: The command set associated with the commands
+    """
+    if withOutput:
+        print '\033[1mRunning ' + commandType + "\033[0m\n"
+    for cmd in listOfCommands:
+        p = subprocess.Popen(cmd,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.wait()
+        if withOutput:
+            print p.stdout.read()
+            print p.stderr.read()
+        
 def runTests(compile):
     """
     Run all of the tests associated with the language product
@@ -27,17 +43,19 @@ def runTests(compile):
     # Get a list of tests to run from the file directory
     listOfJavascriptTests = []
     listOfCTests = []
+    listOfPythonTests = []
     for root, dirs, list_of_files in os.walk(os.getcwd()):
         for f in list_of_files:
             fileName, fileExt = os.path.splitext(f)
-            print fileExt
             if fileExt == ".c":
                 
                 # Save the actual file name and the executable associated with the filename
                 listOfCTests.append((root + "/" + f, f.replace('Test.c', '')))
             elif fileExt == ".js":
                 listOfJavascriptTests.append(root + "/" + f)
-    listOfCompiledExecutables =[]
+            elif fileExt == ".py":
+                if fileName != "run":
+                    listOfPythonTests.append(root + "/" + f)
     if compile:
         
         # Iterate over the tests and create a gcc executable for each test
@@ -45,22 +63,20 @@ def runTests(compile):
             cmd = ['gcc','-I../lib', '-o', executable, testFile]
             p = subprocess.Popen(cmd,stdout=subprocess.PIPE)
             p.wait()
+            
+        # Setup the python bindings
+        listOfCommands = [['python ../setup.py build'], ['python ../setup.py install']]
+        runListOfCommands(listOfCommands)
 
     # Setup the list of compiled executables    
-    listOfCompiledExecutables.extend([executable for _, executable in listOfCTests])
-
+    listOfCompiledTests = [["./" + executable] for _, executable in listOfCTests]
+    listOfJTests = [["mocha", f] for f in listOfJavascriptTests]
+    listOfPTests = [["python",f] for f in listOfPythonTests]
+   
     # Run the compiled executables
-    for c in listOfCompiledExecutables:
-        cmd = ["./" + c]
-        p = subprocess.Popen(cmd)
-        p.wait()
-        
-    # Run the javascript tests
-    for f in listOfJavascriptTests:
-        print f
-        cmd = ["mocha %s" % f]
-        p = subprocess.Popen(cmd)
-        p.wait()
+    runListOfCommands(listOfCompiledTests,withOutput=True, commandType='C Tests')
+    runListOfCommands(listOfJTests,withOutput=True, commandType='Javascript Binding Tests')
+    runListOfCommands(listOfPTests,withOutput=True, commandType='Python Binding Tests')
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("Language TestingFramework")
