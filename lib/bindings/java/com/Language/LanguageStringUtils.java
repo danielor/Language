9dc955e5c76d400055bfcc1f08a466a42a6fe917
Daniel Ortiz <danielor@umich.edu>
2014-12-07 16:10:26 -0500

    Addition of natural number functionality to the java bindings.

--- lib/bindings/java/com/Language/LanguageStringUtils.java
+++ lib/bindings/java/com/Language/LanguageStringUtils.java
@@ -9,6 +9,28 @@ public class LanguageStringUtils{
 	}
 	
 	/**
+	 * A collection of string encodings supported by
+	 * language
+	 */
+	public enum StringEncodings{
+		UTF8_BINARY(0),
+		ASCII(1),
+		ISO_8859_1(2);
+		
+		private int state;
+		private StringEncodings(int s){
+			state = s;
+		}
+		
+		/**
+		 * Get the integer associated with the string encoding
+		 */
+		public int getValue(){
+			return state;
+		}
+	}
+	
+	/**
 	 * Get the length of a java string
 	 * @param str A java string to get the length
 	 * @param enconding The encoding of the java string
@@ -34,6 +56,13 @@ public class LanguageStringUtils{
 	 */
 	public native int lengthEscapedWithEnd(String str, int encoding, String escapedStr, int escapedEncoding,
 			String endStr);
+	
+	/**
+	 * Check if a character sequence is a natural number
+	 * @param str A java string
+	 * @param encoding The encoding of str
+	 */
+	public native boolean isNaturalNumber(String str, int encoding);
 	
 	public static void main(String[] args) {
 		new LanguageStringUtils().length("Hello", 0);  // invoke the native method
